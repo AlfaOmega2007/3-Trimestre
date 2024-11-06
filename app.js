9081362198b91feaacb97b1262d2a4fa2310b2e4
AlfaOmega2007 <142520553+AlfaOmega2007@users.noreply.github.com>
2024-11-06 10:56:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ let container = document.getElementById('container')
   let cartao = document.createElement('article')
 cartao.className = 'cartao'
 
-cartao.innerHTML = '
+cartao.innerHTML = `
 <div class="cartao_conteudo"> 
 <h3>${categoria}</h3>
+<div class="cartao_conteudo_resposta">
+  <p>${pergunta}</p>
+</div>
+<div class="cartao_conteudo_resposta">
+  <p>${resposta}</p>
+</div>
+</div>
+`
+
+container.appendChild(cartao)
 
 }
